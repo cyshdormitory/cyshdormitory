4ae9e98ee6d645f3a68934792f8808563fa1512a
cyshdormitory <62468899+cyshdormitory@users.noreply.github.com>
2020-04-10 23:13:49 +0800

    Update confirm.js

--- assets/js/confirm.js
+++ assets/js/confirm.js
@@ -1,4 +1,30 @@
 function Confirm(){
+    var date = new Date();
+    var dd = date.getDay();
+    var Time = date.getTime();
+    Time /= (1000*60);
+    Time += 480;
+    Time %= 1440;
+    
+    var openTime = [(6*60),(18*60+50),(22*60+20)];
+    var closeTime = [(8*60+30),(20*60),(23*60+10)];
+  
+    if(dd>=1 && dd<=5 && Time>=openTime[0] && Time<=closeTime[0]){
+        send();
+    }
+    else if(dd>=1 && dd<=4 && ((Time>=openTime[1] &&                Time<=closeTime[1])||(Time>=openTime[2] &&  Time<=closeTime[2]))){
+        send();
+    }
+    else if(dd==0 && Time>=openTime[2] && Time<=closeTime[2]){
+        send();
+    }
+    else{
+        alert("請不要亂送資料\n現在不是量體溫的時段");
+        location.href = "index.html";
+    }
+}
+
+function send(){
     var Name = name.join();
     var data = {	    
         'name' : Name,
