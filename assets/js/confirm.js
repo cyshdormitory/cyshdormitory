420d5832fad639b3ab40a3e8a583ef9de580c124
cyshdormitory <62468899+cyshdormitory@users.noreply.github.com>
2020-04-26 02:10:25 +0800

    Update confirm.js

--- assets/js/confirm.js
+++ assets/js/confirm.js
@@ -5,8 +5,8 @@ function Confirm(){
     Time /= (1000*60);
     Time += 480;
     Time %= 1440;
-    
-    var openTime = [(18*60+50),(22*60+20)];
+    send(Time);
+    /*var openTime = [(18*60+50),(22*60+20)];
     var closeTime = [(20*60),(23*60+30)];
     if(dd>=1 && dd<=4 && ((Time>=openTime[0] && Time<=closeTime[0])||(Time>=openTime[1] &&  Time<=closeTime[1]))){
         send(Time);
@@ -17,13 +17,13 @@ function Confirm(){
     else{
         alert("請不要亂送資料\n現在不是量體溫的時段");
         location.href = "index.html";
-    }
+    }*/
 }
 
 function send(Time){
     var Name = name.join(",");
     Time /= 144;
-    if(Time<5){
+    if(Time>5){
         $.ajax({
             type:'get',
             url: "https://script.google.com/macros/s/AKfycbwkiFsWuoc6Kk6h67sxFR3kmykn_Y-gQE2QMf-zsszKtorSbrrw/exec",
