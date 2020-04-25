b5e1381083bfa70d2564ecc844a9b6abbb511e3d
cyshdormitory <62468899+cyshdormitory@users.noreply.github.com>
2020-04-26 01:50:05 +0800

    Update confirm.js

--- assets/js/confirm.js
+++ assets/js/confirm.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function Confirm(){
     var date = new Date();
     var dd = date.getDay();
@@ -9,8 +8,7 @@ function Confirm(){
     
     var openTime = [(18*60+50),(22*60+20)];
     var closeTime = [(20*60),(23*60+30)];
-   send(Time);
-    /*if(dd>=1 && dd<=4 && ((Time>=openTime[0] && Time<=closeTime[0])||(Time>=openTime[1] &&  Time<=closeTime[1]))){
+    if(dd>=1 && dd<=4 && ((Time>=openTime[0] && Time<=closeTime[0])||(Time>=openTime[1] &&  Time<=closeTime[1]))){
         send(Time);
     }
     else if(dd==0 && Time>=openTime[1] && Time<=closeTime[1]){
@@ -19,7 +17,7 @@ function Confirm(){
     else{
         alert("請不要亂送資料\n現在不是量體溫的時段");
         location.href = "index.html";
-    }*/
+    }
 }
 
 function send(Time){
@@ -62,39 +60,3 @@ function send(Time){
         });
     }
 }
-=======
-function Confirm(){
-    var date = new Date();
-    var dd = date.getDay();
-    var Time = date.getTime();
-    Time /= (1000*60);
-    Time += 480;
-    Time %= 1440;
-    
-    var openTime = [(18*60+50),(22*60+20)];
-    var closeTime = [(20*60),(23*60+30)];
-  
-    if(dd>=1 && dd<=4 && ((Time>=openTime[0] && Time<=closeTime[0])||(Time>=openTime[1] &&  Time<=closeTime[1]))){
-        send(Time);
-    }
-    else if(dd==0 && Time>=openTime[1] && Time<=closeTime[1]){
-        send(Time);
-    }
-    else{
-        alert("請不要亂送資料\n現在不是量體溫的時段");
-        location.href = "index.html";
-    }
-}
-
-function send(Time){
-    var Name = name.join(",");
-    Time /= 144;
-        $.ajax({
-            url: "https://script.google.com/macros/s/AKfycbwkiFsWuoc6Kk6h67sxFR3kmykn_Y-gQE2QMf-zsszKtorSbrrw/exec",
-            data:  {
-                'name' : Name
-            },
-        });
-    location.href="index.html";
-}
->>>>>>> 380323b72288db4cd1bdf7434cc1c01f2e056da6
