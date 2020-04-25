c5f7e8f4c642b34bd362a4308f2493652a802cff
cyshdormitory <62468899+cyshdormitory@users.noreply.github.com>
2020-04-26 01:51:25 +0800

    Update input.js

--- assets/js/input.js
+++ assets/js/input.js
@@ -1,22 +1,10 @@
-<<<<<<< HEAD
 var cube = document.getElementById("name");
 function lattice(){
     var trigger = document.check.name.value;
-        if(trigger !== null){
-            var showRoom = document.check.room.value;
-            showRoom = "Room" + showRoom;
-            document.getElementById(showRoom).style.display="block";
-            document.getElementById("buttonArea").style.display="block";
-        }
-=======
-var cube = document.getElementById("name");
-function lattice(){
-    var trigger = document.check.name.value;
-        if(trigger !== null){
-            var showRoom = document.check.room.value;
-            showRoom = "Room" + showRoom;
-            document.getElementById(showRoom).style.display="block";
-            document.getElementById("buttonArea").style.display="block";
-        }
->>>>>>> 380323b72288db4cd1bdf7434cc1c01f2e056da6
+    if(trigger !== null){
+        var showRoom = document.check.room.value;
+        showRoom = "Room" + showRoom;
+        document.getElementById(showRoom).style.display="block";
+        document.getElementById("buttonArea").style.display="block";
+    }
 }
