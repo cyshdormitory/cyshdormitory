ef20c520555266bc12872c7d84f43de437f8f2d8
Weng-john <59682713+Weng-john@users.noreply.github.com>
2020-08-27 20:04:50 +0800

    debug-1

--- assets/js/input.js
+++ assets/js/input.js
@@ -1,4 +1,5 @@
 var cubeName = [];
+var element= new Array(0);
 var content;
 
 document.getElementById("name").onkeypress = function(event){
@@ -28,33 +29,46 @@ document.getElementById("name").onkeypress = function(event){
 function lattice(){
     document.getElementById("name").blur();
     var value = document.getElementById("name").value;
+    var extinct= true;
     var find = false;
-    if(value !== ""){
-        for(var i=0;i<content[0].length;i++){
-            if(content[0][i] == value){
-                var j=i;
-                find = true;
-                while(content[0][j] == value){
-                    cubeName.push(content[1][j]);
-                    j++;
-                }
-                break;
-            }
+    
+    for(var i=0;i<element.length;i++){
+        if(value==element[i]){
+            extinct=false;
         }
+    }
+    
+    if(extinct){
+        if(value !== ""){
+            for(var i=0;i<content[0].length;i++){
+                if(content[0][i] == value){
+                    var j=i;
+                    find = true;
+                    while(content[0][j] == value){
+                        cubeName.push(content[1][j]);
+                        j++;
+                    }
+                    break;
+                }
+            }
       
-        if(find){
-            find=false;
-            document.getElementById("buttonArea").style.display = "block";
-            $("section").after("<div id="+"insert"+" class="+"Room"+"></div>");
-            var insertDiv = document.getElementById("insert");
-            insertDiv.innerHTML = "<header><h1>" + value + "寢人員名單</h1></header>";
-            while(cubeName.length){
-                var dc_input = document.createElement("input");
-                dc_input.type = 'button';
-                dc_input.id = value;
-                dc_input.onclick = person;
-                dc_input.value = cubeName.shift();
-                insertDiv.appendChild(dc_input);
+            if(find){  //create a div
+                find=false;
+                var insert= "insert " + value;
+                document.getElementById("buttonArea").style.display = "block";
+                $("section").after("<div id="+ "insert" +" class="+"Room"+"></div>");
+                var insertDiv = document.getElementById("insert");
+    console.log(value);
+                insertDiv.innerHTML = "<header><h1>" + value + "寢人員名單</h1></header>";
+                while(cubeName.length){
+                    var dc_input = document.createElement("input");
+                    dc_input.type = 'button';
+                    dc_input.id = value;
+                    dc_input.onclick = person;
+                    dc_input.value = cubeName.shift();
+                    insertDiv.appendChild(dc_input);
+                    element.push(value);
+                }
             }
         }
     }
